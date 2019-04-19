d3f8d5bfb19d36b400e6ef290e7a10305278a82e
Raghavasimhan Sankaranarayanan <aavu@gatech.edu>
2019-04-19 21:22:59 +0000

    Added globals

---
+++ Globals.h
@@ -0,0 +1,14 @@
+//
+// Created by nvidia on 4/12/19.
+//
+
+#ifndef SHIMON_GLOBALS_H
+#define SHIMON_GLOBALS_H
+
+enum StrikerModes {
+    Normal, Medium, Tremolo
+};
+
+extern StrikerModes strikerMode;
+
+#endif //SHIMON_GLOBALS_H
